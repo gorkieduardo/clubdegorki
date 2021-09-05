30ff7d920b1b6d84d62a85d65888c0f04740ef28
Eduardo Estrada <ceed.educacion@gmail.com>
2021-09-04 20:36:11 -0600

    nuevo ajuste

---
+++ uno.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
+import ControlPresupuesto from "./components/ControlPresupuesto";
+
+function App() {
+ 
+    </div>
+  );
+}
+
+export default App;
